dc10ebc539c2df29181065688bc42c8fd0a2ed56
Nikita <semenovknikita@gmail.com>
2026-02-10 00:02:56 +0300

    маски столбцов и строк

--- main.go
+++ main.go
@@ -4,13 +4,9 @@ import (
 	"fmt"
 )
 
-func printMatrix(matrix [64]uint64) {
-	for i := 0; i < len(matrix); i++ {
-		fmt.Print(matrix[i])
-		if (i+1)%8 == 0 {
-			fmt.Println()
-		}
-	}
+type Position struct {
+	WP, WN, WB, WR, WQ, WK uint64
+	BP, BN, BB, BR, BQ, BK uint64
 }
 
 func bitboardToArray(bb uint64) (board [64]uint8) {
@@ -18,6 +14,25 @@ func bitboardToArray(bb uint64) (board [64]uint8) {
 		board[i] = uint8((bb >> i) & 1)
 	}
 	return
+}
+
+func InitStartPos() Position {
+	const (
+		Rank1 uint64 = 0x00000000000000FF
+		Rank2 uint64 = 0x000000000000FF00
+		Rank7 uint64 = 0x00FF000000000000
+		Rank8 uint64 = 0xFF00000000000000
+
+		FileA uint64 = 0x0101010101010101
+		FileB uint64 = 0x0202020202020202
+		FileC uint64 = 0x0404040404040404
+		FileD uint64 = 0x0808080808080808
+		FileE uint64 = 0x1010101010101010
+		FileF uint64 = 0x2020202020202020
+		FileG uint64 = 0x4040404040404040
+		FileH uint64 = 0x8080808080808080
+	)
+	return *&Position{}
 }
 
 func main() {
@@ -28,6 +43,7 @@ func main() {
 
 	var whitePawns uint64 = 1<<a2 | 1<<b2 | 1<<c2 | 1<<d2 | 1<<e2 | 1<<f2 | 1<<g2 | 1<<h2
 	var blackPawns uint64 = 1<<a7 | 1<<b7 | 1<<c7 | 1<<d7 | 1<<e7 | 1<<f7 | 1<<g7 | 1<<h7
+
 	//fmt.Printf("%b \n", board)
 	//board = board &^ 0b111011
 	fmt.Printf("%b \n", whitePawns)
@@ -40,4 +56,6 @@ func main() {
 	//fmt.Println()
 	matrix := bitboardToArray(whitePawns)
 	fmt.Println(matrix)
+
+	fmt.Printf("%b", (0x0101010101010101|0x8080808080808080)&0x00000000000000FF)
 }
